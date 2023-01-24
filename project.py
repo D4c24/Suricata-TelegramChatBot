6404965c016a2eb0b5ff9cb8ab84e45a67c7bdb5
David <callejasdavid16@gmail.com>
2023-01-24 04:03:55 +0400

    Creation of def to read line by line the log file and using regex get only alerts with priority higher than 3

--- project.py
+++ project.py
@@ -1,5 +1,19 @@
+import os, re
+
 def run():
-  pass
+  file = open('data-sample.log', 'r')
+  lines = file.readlines()
+
+  count = 0
+  #regex = re.search("Priority: [3-9]". line)
+
+  for line in lines:
+    count += 1
+    regex = str(re.search("Priority: [3-9]", line))
+    if (regex == "None"):
+      pass
+    else:
+      print('line: ' + str(count) + ' - ' + line)
 
 if __name__ == '__main__':
   run()
