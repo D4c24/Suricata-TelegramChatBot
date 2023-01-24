0792997bb7653433ec799d47db8d3c58ad45215c
David <callejasdavid16@gmail.com>
2023-01-24 05:02:42 +0400

    first test enabling telegram chatbot

--- project.py
+++ project.py
@@ -1,11 +1,20 @@
-import os, re
+import os, re, telegram
 
+##HTTP API from telegrambot - BotFather
+chat_id = '902953144'
+api_key = '5917426187:AAEI7WL8HNeJGs0Az37mYOTRK9DaWHMPXxc'
+
+##Function to send messages to telegram
+def bot(msg):
+  bot = telegram.Bot(token=api_key)
+  bot.send_message(chat_id=chat_id, text=str(msg))
+
+## Main Function, reads file and uses regex to match only Suricata alerts with priority higher than 3
 def run():
   file = open('data-sample.log', 'r')
   lines = file.readlines()
 
   count = 0
-  #regex = re.search("Priority: [3-9]". line)
 
   for line in lines:
     count += 1
@@ -13,7 +22,10 @@ def run():
     if (regex == "None"):
       pass
     else:
-      print('line: ' + str(count) + ' - ' + line)
+      #msg = 'Suricata-Alert: ' + str(count) + ' - ' + line
+      msg = 'test pepito'
+      bot(msg)
+      #print('line: ' + str(count) + ' - ' + line)
 
 if __name__ == '__main__':
   run()
